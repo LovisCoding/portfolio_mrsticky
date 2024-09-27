1fdad5ea2233b804095629e7f0a03fb2278d34b5
LovisCoding <arthur.lecomte@etu.univ-lehavre.fr>
2024-09-27 10:36:23 +0200

    bug fix

--- src/components/Misc/SectionImg.jsx
+++ src/components/Misc/SectionImg.jsx
@@ -10,11 +10,11 @@ export default function SectionImg({title,imgs, id, key}) {
 	let md = 12
 	if (id === 'pp') md = 15
 	return (
-		<Stack alignItems={'center'} display={'flex'} id={id} spacing={3} pt={4} >
+		<Stack alignItems={'center'} display={'flex'} id={id} spacing={3} pt={4} width={"100%"}>
 			{title ? (
 				<Typography variant={'h2'} sx={{textAlign: 'center', color: 'white'}}>{title}</Typography>
 			): ''}
-			<Grid container rowSpacing={5} columns={{ xs: 4, sm: 8, md: md }} columnSpacing={smDown ? 0 : 2} maxWidth={'100%'} display={'flex'} justifyContent={'center'} >
+			<Grid container rowSpacing={5} columns={{ xs: 4, sm: 8, md: md }} columnSpacing={smDown ? 0 : 2} maxWidth={'100%'} height={'100%'} display={'flex'} justifyContent={'center'} >
 				{Object.keys(imgs).map((img, index) => (
 					<DisplayImage img={imgs[img]} index={index} id={id} key={index}/>
 				))}
